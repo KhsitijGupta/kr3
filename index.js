ccb5d9d72e687ffc2fb6f13aac918e6e560dc3ff
Kshitij Gupta <kshitijgupta2162@gmail.com>
2024-10-16 12:43:37 +0530

    for uploading

--- index.js
+++ index.js
@@ -51,7 +51,9 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    DB_PORT:3306
+    DB_PORT:3306,
+    fatal: false
+
 });
 
 connection.connect((err) => {
