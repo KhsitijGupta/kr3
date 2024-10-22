5e497be85e846859c728198976fee3c8ecec28ce
Raushan Gupta <raushangupta1425@gmail.com>
2024-10-22 13:04:08 +0530

    login working

--- public/js/loginRegister.js
+++ public/js/loginRegister.js
@@ -6,17 +6,26 @@ let register = document.querySelector(".register-link");
 let close = document.querySelector(".icon-close");
 let close2 = document.querySelector(".close2");
 
-// btn.addEventListener("click",()=>{
-//     ctn.style="transform:scale(1);"
-//     ctn1.style="display:none;"
-// })
-// close.addEventListener("click",()=>{
-//     ctn.style="transform:scale(0);"
-// })
-// close2.addEventListener("click",()=>{
-//     ctn1.style="display:none;"
-//     ctn.style="display:none;"
-// })
+/*Login box management.*/
+let userAccount = document.getElementById('userAccount');
+let instituteAccount = document.getElementById('instituteAccount');
+let userLoginControl = document.querySelector('.userLoginControl');
+let instituteLoginControl = document.querySelector('.instituteLoginControl');
+
+userAccount.addEventListener('click', () => {
+    instituteLoginControl.style.display = "none";
+    userLoginControl.style.display = "block";
+    instituteAccount.classList.remove('tabActive');
+    userAccount.classList.add('tabActive');
+})
+instituteAccount.addEventListener('click', () => {
+    userLoginControl.style.display = "none";
+    instituteLoginControl.style.display = "block";
+    userAccount.classList.remove('tabActive');
+    instituteAccount.classList.add('tabActive');
+})
+
+
 register.addEventListener("click",()=>{
     ctn.style="display:none;"
     ctn1.style="transform:scale(1);"
