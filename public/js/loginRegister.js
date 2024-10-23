059c6901e73b028b870b2b39b0613f57a14c3752
Raushan Gupta <raushangupta1425@gmail.com>
2024-10-23 08:13:44 +0530

    login new look

--- public/js/loginRegister.js
+++ public/js/loginRegister.js
@@ -1,11 +1,3 @@
-let ctn = document.querySelector(".containerlgn");
-let ctn1 = document.querySelector(".container1");
-let login = document.querySelector(".login-link");
-let register = document.querySelector(".register-link");
-// let btn = document.querySelector(".btnLoginPopup");
-let close = document.querySelector(".icon-close");
-let close2 = document.querySelector(".close2");
-
 /*Login box management.*/
 let userAccount = document.getElementById('userAccount');
 let instituteAccount = document.getElementById('instituteAccount');
@@ -25,12 +17,17 @@ instituteAccount.addEventListener('click', () => {
     instituteAccount.classList.add('tabActive');
 })
 
+// switch login page and register
+let loginPage = document.querySelector('.loginPage');
+let registerPage = document.querySelector('.registerPage');
+let registerLink = document.querySelector('.register-link');
+let loginLink = document.querySelector('.login-link');
 
-register.addEventListener("click",()=>{
-    ctn.style="display:none;"
-    ctn1.style="transform:scale(1);"
-})
-login.addEventListener("click",()=>{
-    ctn1.style="display:none;"
-    ctn.style="transform:scale(1);"
-})
+registerLink.addEventListener("click",()=>{
+    loginPage.style="display:none;"
+    registerPage.style="transform:scale(1);"
+});
+loginLink.addEventListener("click",()=>{
+    registerPage.style="display:none;"
+    loginPage.style="transform:scale(1);"
+});
