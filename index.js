c8909d9daeec6d32914eaa1b7029030c22702e3b
Kshitijgupta1728 <130221321+KhsitijGupta@users.noreply.github.com>
2025-05-06 11:08:02 +0530

    Update admin panel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const path = require("path");
